33ec0d37cfe87d3ac25ac6c0cfd1eca513c4fc5a
dario <darioasaro29@gmail.com>
2020-03-05 15:08:08 -0300

    delete other proy

--- js/modules/app.js
+++
@@ -1,6 +0,0 @@
-import { pi } from "./math.js";
-import { risas } from "./risas.js"
-
-console.log(pi);
-
-console.log(risas.reirFuerte());
--- node/express-ejemplo/routes/routes.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = routes = ( app ) => {
-  app.use('/api/tasks', require( './tasks' ))
-  // app.use('/api/users', require( './users' )),
-}
